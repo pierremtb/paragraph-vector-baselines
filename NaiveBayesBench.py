3b256a28e5ef41839014c361afed1f37b83b7b97
Michael Segev <msegev@gmail.com>
2019-04-11 15:47:30 -0400

    Switched to Multinomial NB so features don't need to be binary

--- NaiveBayesBench.py
+++ NaiveBayesBench.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import BernoulliNB
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
 from sklearn import metrics
 
 dataPath = "./stanfordSentimentTreebank/"
@@ -11,8 +11,8 @@ dataPath = "./stanfordSentimentTreebank/"
 print("Opening files...")
 cdp = ClassifierDataPrepper(dataPath)
 
-# x_train, y_train, x_valid, y_valid, x_test, y_test = cdp.getXYlabeledBinary()
-x_train, y_train, x_valid, y_valid, x_test, y_test = cdp.getXYlabeledSplit()
+# x_train, y_train, x_valid, y_valid, x_test, y_test = cdp.getXYlabeledBinary()  # best score is 0.7823529411764706
+x_train, y_train, x_valid, y_valid, x_test, y_test = cdp.getXYlabeledSplit()  # best score is 0.39547511312217193
 
 # uncomment this to use test set!
 x_valid = x_test
@@ -22,9 +22,9 @@ y_valid = y_test
 
 # Building different vectorizers used to parse the text into features
 print("Extracting features from data ...")
-vectBinCount = CountVectorizer(min_df=1, ngram_range=(1, 2), binary=True)
-vectCount = CountVectorizer(min_df=1, ngram_range=(1, 2), binary=False)
-vectTfidf = TfidfVectorizer(min_df=1, ngram_range=(1, 2))
+vectBinCount = CountVectorizer(ngram_range=(1, 2), binary=True)
+vectCount = CountVectorizer(ngram_range=(1, 2), binary=False)
+vectTfidf = TfidfVectorizer(ngram_range=(1, 2))
 
 # learn the vocabularies from training data for each vector type
 vectBinCount.fit(x_train)
@@ -43,13 +43,13 @@ X_validate_tfidf = vectTfidf.transform(x_valid)
 
 # create and train Logistic Regression model for each type of vectorizer
 print("Training models...")
-model_binCount = BernoulliNB()  # MultinomialNB()
+model_binCount = MultinomialNB()
 model_binCount.fit(X_train_binCount, y_train)
 
-model_count = BernoulliNB()  # MultinomialNB()
+model_count = MultinomialNB()
 model_count.fit(X_train_count, y_train)
 
-model_tfidf = BernoulliNB()  # MultinomialNB()
+model_tfidf = MultinomialNB()
 model_tfidf.fit(X_train_tfidf, y_train)
 
 # run models on validation set
